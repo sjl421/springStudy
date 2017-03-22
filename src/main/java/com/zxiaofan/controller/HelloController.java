9f5b473a435b3c361fe041a382315216e82e1013
zxiaofan <zeng-dream@live.com>
2017-03-23 00:58:55 +0800

    StudySpringBoot_属性配置；开发生产环境的区分

--- src/main/java/com/zxiaofan/controller/HelloController.java
+++ src/main/java/com/zxiaofan/controller/HelloController.java
@@ -1,5 +1,8 @@
 package com.zxiaofan.controller;
 
+import com.zxiaofan.properties.CarProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,12 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
+    @Value("${fruit.apple}")
+    private String fruitApple;
+    @Value("${fruits}")
+    private String fruits;
+    @Autowired
+    private CarProperties carProperties;
+
     /**
      * 访问127.0.0.1:8081/hi
+     *
      * @return hi
      */
-    @RequestMapping(value = "/hi",method = RequestMethod.GET)
-    public String hi(){
-        return "Hi SpringBoot , zxiaofan.com";
+    @RequestMapping(value = "/hi", method = RequestMethod.GET)
+    public String hi() {
+        String result = "Hi SpringBoot , zxiaofan.com";
+        return result + "_" + fruitApple + "_" + fruits + "_car_" + carProperties.getColor();
     }
 }
