dd994314246560f219ce6b3b4c0a4827c79432bb
zxiaofan <zeng-dream@live.com>
2017-03-26 22:12:57 +0800

    StudySpringBoot_Controller相关注解的使用

--- src/main/java/com/zxiaofan/controller/HelloController.java
+++ src/main/java/com/zxiaofan/controller/HelloController.java
@@ -3,14 +3,13 @@ package com.zxiaofan.controller;
 import com.zxiaofan.properties.CarProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by xiaofan on 2017/3/22.
  */
 @RestController
+//@RequestMapping("/class") // 给类指定路径
 public class HelloController {
     @Value("${fruit.apple}")
     private String fruitApple;
@@ -24,9 +23,15 @@ public class HelloController {
      *
      * @return hi
      */
-    @RequestMapping(value = "/hi", method = RequestMethod.GET)
-    public String hi() {
+    @RequestMapping(value = "/hi/{id}", method = RequestMethod.GET)
+//    @RequestMapping(value = {"/hi","/hello"}, method = RequestMethod.GET)
+    public String hi(@PathVariable("id") Integer idParam) {
         String result = "Hi SpringBoot , zxiaofan.com";
-        return result + "_" + fruitApple + "_" + fruits + "_car_" + carProperties.getColor();
+        return result + "_" + fruitApple + "_" + fruits + "_car_" + carProperties.getColor()+"_(id:"+idParam+")";
+    }
+//    @RequestMapping(value = "/good",method = RequestMethod.GET)
+    @GetMapping(value="/good") // 注解简化
+    public String good(@RequestParam(value = "id",required = false,defaultValue = "0") Integer idParam) {
+        return "id:"+idParam;
     }
 }
